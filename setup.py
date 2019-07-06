3fadc6d9953597ae9b11340442c8aa7bdb3dc108
Hiroyuki Ohsaki <code@lsnl.jp>
2019-07-07 00:22:40 +0900

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     url="https://github.com/h-ohsaki/graph-tools",
     packages=setuptools.find_packages(),
     install_requires=['numpy', 'perlcompat', 'pytess'],
-    scripts=[''],
+#    scripts=[''],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
