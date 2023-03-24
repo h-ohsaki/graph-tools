7b908769756d22d3c953435b25a994aefd9dd589
Hiroyuki Ohsaki <code@lsnl.jp>
2023-03-25 04:32:53 +0900

    add undirected case

--- test/81_floydwarshall.py
+++ test/81_floydwarshall.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import math
+
 from test_more import ok
 from graph_tools import Graph
 
@@ -11,6 +13,18 @@ g.floyd_warshall()
 ok(g.T[1][2] == 1)
 ok(g.T[2][3] == 1)
 ok(g.T[1][3] == 2)
-ok(not g.T[2].get(1, None))
-ok(not g.T[3].get(2, None))
-ok(not g.T[3].get(1, None))
+ok(g.T[2][1] == math.inf)
+ok(g.T[3][2] == math.inf)
+ok(g.T[3][1] == math.inf)
+
+g = Graph(directed=False)
+
+g.add_edge(1, 2)
+g.add_edge(2, 3)
+g.floyd_warshall()
+ok(g.T[1][2] == 1)
+ok(g.T[2][3] == 1)
+ok(g.T[1][3] == 2)
+ok(g.T[2][1] == 1)
+ok(g.T[3][2] == 1)
+ok(g.T[3][1] == 2)
