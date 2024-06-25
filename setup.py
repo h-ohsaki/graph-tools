d19cd6c3c2d850af5122311b6b2045f91b4c20f7
Hiroyuki Ohsaki <code@lsnl.jp>
2024-06-25 14:33:48 +0900

    version 1.14

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="graph-tools",
-    version="1.13",
+    version="1.14",
     author="Hiroyuki Ohsaki",
     author_email="ohsaki@lsnl.jp",
     description=
